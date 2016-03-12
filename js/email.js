c420b49c726a199d09ba083b2d3cc53938a1b287
luist <luistrigueiros@gmail.com>
2016-03-12 23:47:54 +0000

    Publish of Github pages from Gradle.

--- js/email.js
+++ js/email.js
@@ -1,40 +1,37 @@
-$( document ).ready(function() {
-    //https://medium.com/@mariusc23/send-an-email-using-only-javascript-b53319616782#.716pmd482
-    //https://mandrillapp.com/api/docs/messages.html
-    var requestData = {
-        "key": "MvLSH6-M1BNq8w4VGlaekw",
-        "message": {
-            "html": "<p>Example HTML content</p>",
-            "text": "Example text content",
-            "subject": "example subject",
-            "from_email": "luistrigueiros@gmail.com",
-            "from_name": "Luis Trigueiros",
-            "to": [
-                {
-                    "email": "luistrigueiros@gmail.com",
-                    "name": "Recipient Name",
-                    "type": "to"
-                }
-            ],
-            "headers": {
-                "Reply-To": "luistrigueiros@gmail.com"
-            },
-            "auto_text": null,
-            "auto_html": null
-            },
-        "async": false
-    };
 
-    var ajaxConfObj = {
+function buildRequestData() {
+    return {
+        from:'Mailgun Sandbox <postmaster@sandbox1f1c8a6d340244db94f21f52fc273de8.mailgun.org>',
+        to:'luistrigueiros <luistrigueiros@gmail.com>',
+        subject: 'Hello luistrigueiros',
+        text:'Congratulations luistrigueiros, you just sent an email with Mailgun!  You are truly awesome!'
+    };
+}
+
+function buildAjaxConf() {
+    var errorFuncObj = function (xhr, ajaxOptions, thrownError) {
+       console.log("Request failed with status" + xhr.status);
+       console.log(thrownError);
+    };
+    var basicAuthFuncObj = function(xhr) {
+        xhr.setRequestHeader ("Authorization", "Basic " + btoa("api:key-2a3c50e54d7747aec34a27feb1a9e35c"));
+    };
+    var successFuncObj = function(data) {
+        console.log(data);
+        $("#myModal").modal();
+    };
+    return {
         type: "POST",
-        url: "https://mandrillapp.com/api/1.0/messages/send.json",
-        data: requestData
+        url: "https://api.mailgun.net/v3/sandbox1f1c8a6d340244db94f21f52fc273de8.mailgun.org/messages",
+        error:  errorFuncObj,
+        success: successFuncObj,
+        beforeSend: basicAuthFuncObj,
+        data: buildRequestData()
      };
-    var doneFuncObj = function(response) {
-        console.log(response);
-     };
-    var clickFuncObj = function() {
-        $.ajax(ajaxConfObj).done(doneFuncObj);
-     };
-    $('#sendEmailBtn').click(clickFuncObj);
+}
+
+$( document ).ready(function() {
+    $("#sendEmailBtn").click(function(){
+        $.ajax(buildAjaxConf());
+    });
 });
